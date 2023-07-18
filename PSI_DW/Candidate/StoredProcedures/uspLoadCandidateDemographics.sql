59183259c7dfd75ff75b15ca88fecd285034e50d
psiservices-BruceGilpin <Bruce.Gilpin@psionline.com>
2023-07-18 11:07:35 -0500

    New Stored proc

--- PSI_DW/Candidate/StoredProcedures/uspLoadCandidateDemographics.sql
+++ PSI_DW/Candidate/StoredProcedures/uspLoadCandidateDemographics.sql
@@ -2,34 +2,104 @@
 
 AS
 Begin
-	declare @demotype nvarchar(3), @democolumn nvarchar(40), @statement NVarchar(2000), @variabletypes varchar(1500)
-	declare c1 cursor for
-		Select distinct DemographicTypeDBId, CandidateColumnName
-		from Candidate.DemographicTypes
-	Open C1
-	Fetch Next from C1 into @demotype, @democolumn
-	While @@FETCH_STATUS = 0
-	Begin
-		set @statement = 
-		'With DemoValues (Candidatedbid, Democode, DemographicID) ' +
-		'as ( '+
-		'select  a.candidatedbid, b.variableID, c.DemographicId from ' +
-		'Candidate.Candidates_vw a join ' +
-		'PSI_Staging.Staging.CandidateAdditionalAttributeValues b on ' +
-			'a.SourceCandidateKey = try_cast(b.TestRegnID as varchar(20)) join ' +
-		'Candidate.Demographics c on ' +
-			'b.variableID = c.DemographicTypeKey join ' +
-		'Candidate.DemographicTypes d on ' +
-		'c.DemographicTypeKey = d.SourceAttributeId ' +
-		'Where d.demographicTypeDBID = ' + @demotype + ' )' +
-		'Update Candidate.Candidates set ' + @democolumn + ' = a.DemographicID ' +
-		'From ' +
-			'DemoValues a join ' +
-			'Candidate.Candidates b on ' +
-				'a.candidateddbid = b.candidatedbid and b.currentflag = 0'
-		Execute sp_executesql @statement
-		Fetch Next from C1 into @demotype, @democolumn
+Declare @demographic varchar(50), @CandidateColumnName varchar(40), @sql1 varchar(1500), @Sql2 varchar(500)
+
+set @sql1 = 
+	'with demographic  (TestRegnKey, DemoGraphicID) as
+(select 
+
+	 b.TestRegnKey, d.DemographicId
+from
+	candidate.candidates_vw a join
+	psi_staging.staging.candidateadditionalattributevalues b 
+	on
+	a.testregn_id = b.testregnkey join
+	candidate.demographictypes c on	
+		b.VariableID = c.sourceattributeid join
+	Candidate.Demographics d on
+		b.VariableValue = d.DemographicValue
+where c.demographic = '
+Begin try
+		declare c1 cursor for
+			Select distinct demographic, CandidateColumnName
+			from Candidate.DemographicTypes
+		open c1
+		Fetch Next from c1 into @demographic, @CandidateColumnName
+		while @@FETCH_STATUS = 0
+		Begin
+
+		set @sql1 = 
+			'with demographic  (TestRegnKey, DemoGraphicID) as
+		(select 
+
+			 b.TestRegnKey, d.DemographicId
+		from
+			candidate.candidates_vw a join
+			psi_staging.staging.candidateadditionalattributevalues b 
+			on
+			a.testregn_id = b.testregnkey join
+			candidate.demographictypes c on	
+				b.VariableID = c.sourceattributeid join
+			Candidate.Demographics d on
+				b.VariableValue = d.DemographicValue
+		where c.demographic = ' + '''' + @demographic +  '''' +
+			' )
+		update Candidate.Candidates
+			set ' +  @CandidateColumnName + 
+		' = b.DemographicID 
+		from
+			Candidate.Candidates a join
+			demographic b on 
+				try_cast(a.SourceCandidateKey as numeric) = b.TestRegnKey and
+				a.CurrentFlag = 0'
+
+
+			Exec (@sql1)
+			Fetch Next from c1 into @demographic, @CandidateColumnName
 	end
+	end try
+	Begin catch
+		Insert Logging.SQLErrors (
+			ErrorLIne,
+			ErrorMessage,
+			ErrorNumber,
+			ErrorProcedure,
+			ErrorSeverity,
+			ErrorState,
+			ErrorDateTime )
+		Values (
+			ERROR_LINE(),
+			ERROR_MESSAGE(),
+			ERROR_NUMBER(),
+			ERROR_PROCEDURE(),
+			ERROR_SEVERITY(),
+			ERROR_STATE(),
+			getdate() )
+	end catch
+
+-- Update Statement
+--	with demographic  (TestRegnKey, DemoGraphicID) as
+--(select 
+
+--	 b.TestRegnKey, d.DemographicId
+--from
+--	candidate.candidates_vw a join
+--	psi_staging.staging.candidateadditionalattributevalues b 
+--	on
+--	a.testregn_id = b.testregnkey join
+--	candidate.demographictypes c on	
+--		b.VariableID = c.sourceattributeid join
+--	Candidate.Demographics d on
+--		b.VariableValue = d.DemographicValue
+--where c.demographic = 'race' )
+--update Candidate.Candidates
+--	set RaceCode = b.DemographicID
+--from
+--	Candidate.Candidates a join
+--	demographic b on 
+--		try_cast(a.SourceCandidateKey as numeric) = b.TestRegnKey and
+--		a.CurrentFlag = 0	
+	
 end		
 
 
