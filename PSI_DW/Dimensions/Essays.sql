e925e8e8cfc131b1bd0b7194d3ffe98876c5ebab
psiservices-BruceGilpin <Bruce.Gilpin@psionline.com>
2023-09-07 09:45:11 -0500

    New tables and columns

---
+++ PSI_DW/Dimensions/Essays.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [Dimensions].[Essays]
+(
+	[EssayId] BIGINT NOT NULL Identity(1,1) PRIMARY KEY, 
+    [StudentResponseKey] BIGINT NOT NULL, 
+    [Essay] VARCHAR(MAX) NOT NULL, 
+    [CurrentFlag] BIT NOT NULL DEFAULT 0, 
+    [LoadDate] DATETIME NOT NULL DEFAULT getdate(), 
+    [Checksum] BIGINT NOT NULL
+)
+
+GO
+
+CREATE INDEX [IX_Essays_CurrentFlag] ON [Dimensions].[Essays] ([CurrentFlag])
+Include (StudentResponseKey)
---
+++ PSI_DW/Dimensions/StudentResponses.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE [Dimensions].[StudentResponses]
+(
+	[StudentResponseId] BIGINT NOT NULL Identity(1,1) PRIMARY KEY, 
+    [StudentItemKey] BIGINT NOT NULL,
+    [NumericChoice] SMALLINT NULL, 
+    [StringChoice] VARCHAR(5) NULL, 
+    [ResponseIndex] int null,
+    [Points] DECIMAL(6, 2) NULL, 
+    [StudentResponseDBID] BIGINT NULL, 
+    [CurrentFlag] BIT NOT NULL DEFAULT 0, 
+    [Checksum] BIGINT NULL, 
+    [LoadDate] DATETIME NOT NULL DEFAULT getdate()
+)
+
+GO
+
+CREATE INDEX [IX_StudentResponses_CurrentFlag] ON [Dimensions].[StudentResponses] ([CurrentFlag])
+Include (StudentResponseDBID)
