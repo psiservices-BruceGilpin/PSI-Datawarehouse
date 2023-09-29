28e145b1b6a113cf39a8b8c62c1a3aa9a0cfff16
psiservices-BruceGilpin <Bruce.Gilpin@psionline.com>
2023-09-29 09:20:44 -0500

    Two new views

---
+++ PSIReporting/Dimensions/RecertificationTrue_vw.sql
@@ -0,0 +1,14 @@
+﻿CREATE VIEW [Dimensions].[RecertificationTrue_vw]
+	AS SELECT 
+		a.StudentScoresKey,
+		c.PoolTitle as Reapplicant
+	from
+		[$(PSI_DW)].Dimensions.StudentCodes  (nolock) a		join
+		[$(PSI_DW)].Dimensions.PoolCodeGroups (nolock) b on
+			a.PoolCodeGroupKey = b.PoolCodeGroupDBID  and
+			b.CurrentFlag = 0								join
+		[$(PSI_DW)].Dimensions.PoolCodes c on
+			a.PoolCode = c.PoolCodeDBID and c.CurrentFlag = 0
+	where
+		b.PoolGroupTitle = 'Recertification' and
+		c.PoolTitle = 'Y'
