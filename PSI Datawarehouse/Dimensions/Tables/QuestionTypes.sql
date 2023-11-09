a93f02c0e21e926ebdcb40c8ba947a7827490155
psiservices-BruceGilpin <Bruce.Gilpin@psionline.com>
2023-11-09 14:28:38 -0600

    Two new table

---
+++ PSI Datawarehouse/Dimensions/Tables/QuestionTypes.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [Dimensions].[QuestionTypes]
+(
+	[QuestionTypeId] INT NOT NULL PRIMARY KEY, 
+    [QuestionTypeDBID] INT NOT NULL, 
+    [CurrentFlag] BIT NOT NULL DEFAULT 0, 
+    [QuestionTypeTitle] VARCHAR(10) NOT NULL, 
+    [QuestionTypeDesc] VARCHAR(60) NULL, 
+    [Checksum] BIGINT NOT NULL, 
+    [Createdate] DATETIME NOT NULL DEFAULT getdate()
+)
