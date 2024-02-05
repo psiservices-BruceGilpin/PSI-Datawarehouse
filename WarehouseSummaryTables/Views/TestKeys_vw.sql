2faa50823b5371782d7a59f3ad1a72a05d1708d5
psiservices-BruceGilpin <Bruce.Gilpin@psionline.com>
2024-02-05 11:18:49 -0600

    Database reference changed

--- WarehouseSummaryTables/Views/TestKeys_vw.sql
+++ WarehouseSummaryTables/Views/TestKeys_vw.sql
@@ -1,12 +1,12 @@
-﻿--CREATE VIEW [Tests].[TestKeys_vw]
---	AS SELECT 
---		a.ClientCode,
---		b.Testdbid,
---		b.Testdate
---	From 
---		[$(PSI_DW)].Dimensions.Clients a 
---		join
---		[$(PSI_DW)].Dimensions.Pools b on
---			a.PoolKey = b.PoolDbID
---		join
---		[$(PSI_DW)].
+﻿CREATE VIEW [Tests].[TestKeys_vw]
+as
+select distinct b.AreaTitle Account, d.SummaryTestName TestName
+from [$(PSIReporting)].dimensions.TestPackages_vw a join
+[$(PSIReporting)].dimensions.areas_vw b on
+	a.Areakey = b.areadbid
+join
+[$(PSIReporting)].dimensions.TestLists_vw c on
+	a.TestPackageDBID = c.TestPackageKey 
+join
+tests.summarytestnames d on
+	c.testkey = d.dimensionstestkey
