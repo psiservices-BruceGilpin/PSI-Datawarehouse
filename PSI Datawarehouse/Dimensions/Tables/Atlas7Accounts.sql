f283e9f70f434402908d6d34d5d846b9d2c5512f
psiservices-BruceGilpin <Bruce.Gilpin@psionline.com>
2024-05-22 10:58:45 -0500

    New table for Atlas7 CLients

---
+++ PSI Datawarehouse/Dimensions/Tables/Atlas7Accounts.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dimensions].[Atlas7Accounts]
+(
+	[Atlas7AccountId] INT NOT NULL identity(1,1) PRIMARY KEY, 
+    [Atlas7ClientID] INT NOT NULL, 
+    [Atlas7ClientCode] VARCHAR(50) NOT NULL, 
+    [PoolKey] INT NOT NULL, 
+    [AtlasAccountCode] VARCHAR(32) NULL, 
+    [AtlasAccountID] INT NULL, 
+    [AreaKey] INT NULL, 
+    [Checksum] BIGINT NULL, 
+    [Loaddate] DATETIME NOT NULL DEFAULT getdate(), 
+    [CurrentFlag] TINYINT NOT NULL DEFAULT 0, 
+    [Atlas7AccountDBID] INT NULL
+)
